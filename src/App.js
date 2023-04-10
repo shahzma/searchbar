cd64a23442a60fa11a15984e66f313340333fe70
shahzma <shahzmaalif@gmail.com>
2023-04-10 11:45:55 +0530

    uodate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+
 
 function App() {
+  let data = [
+    {
+      "id": "123-s2-546",
+      "name": "John Jacobs",
+      "items": ["bucket", "bottle"],
+      "address": "1st Cross, 9th Main, abc Apartment",
+      "pincode": "5xx012"
+    },
+    {
+      "id": "123-s3-146",
+      "name": "David Mire",
+      "items": ["Bedroom Set"],
+      "address": "2nd Cross, BTI Apartment",
+      "pincode": "4xx012"
+    },
+    {
+      "id": "223-a1-234",
+      "name": "Soloman Marshall",
+      "items": ["bottle"],
+      "address": "Riverbed Apartment",
+      "pincode": "4xx032"
+    },
+    {
+      "id": "121-s2-111",
+      "name": "Ricky Beno",
+      "items": ["Mobile Set"],
+      "address": "Sunshine City",
+      "pincode": "5xx072"
+    },
+    {
+      "id": "123-p2-246",
+      "name": "Sikander Singh",
+      "items": ["Air Conditioner"],
+      "address": "Riverbed Apartment",
+      "pincode": "4xx032"
+    },
+    {
+      "id": "b23-s2-321",
+      "name": "Ross Wheeler",
+      "items": ["Mobile"],
+      "address": "1st Cross, 9th Main, abc Apartement",
+      "pincode": "5xx012"
+    },
+    {
+      "id": "113-n2-563",
+      "name": "Ben Bish",
+      "items": ["Kitchen Set", "Chair"],
+      "address": "Sunshine City",
+      "pincode": "5xx072"
+    },
+    {
+      "id": "323-s2-112",
+      "name": "John Michael",
+      "items": ["Refrigerator"],
+      "address": "1st Cross, 9th Main, abc Apartement",
+      "pincode": "5xx012"
+    },
+    {
+      "id": "abc-34-122",
+      "name": "Jason Jordan",
+      "items": ["Mobile"],
+      "address": "Riverbed Apartment",
+      "pincode": "4xx032"
+    }
+  ]
+
+  const handleOnSearch = (string, results) => {
+    // onSearch will have as the first callback parameter
+    // the string searched and for the second the results.
+    console.log(string, results)
+  }
+
+  const handleOnHover = (result) => {
+    // the item hovered
+    console.log(result)
+  }
+
+  const handleOnSelect = (item) => {
+    // the item selected
+    console.log(item)
+  }
+
+  const handleOnFocus = () => {
+    console.log('Focused')
+  }
+
+  const formatResult = (item) => {
+    return (
+      <>
+        <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
+        <span style={{ display: 'block', textAlign: 'left' }}>name: {item.name}</span>
+        <span style={{ display: 'block', textAlign: 'left' }}>address: {item.address}</span>
+        <span style={{ display: 'block', textAlign: 'left' }}>pincode: {item.pincode}</span>
+      </>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ width: 800 , marginTop:'25vh',marginLeft:'20vw'}}>
+          <ReactSearchAutocomplete
+            items={data}
+            fuseOptions={{ keys: ["id", "name", 'address', 'pincode'] }}
+            onSearch={handleOnSearch}
+            onHover={handleOnHover}
+            onSelect={handleOnSelect}
+            onFocus={handleOnFocus}
+            autoFocus
+            formatResult={formatResult}
+          />
+        </div>
   );
 }
 
